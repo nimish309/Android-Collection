9cf0382d1dad893c148befa2cb94735c36894d3a
nimish309 <nimishbhoraniya007@gmail.com>
2015-12-22 21:09:55 +0530

    Create Deprecated.java

---
+++ Deprecated.java
@@ -0,0 +1,29 @@
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+
+public class Deprecated {
+
+    private Activity activity;
+
+    public Deprecated(Activity activity) {
+        this.activity = activity;
+    }
+
+    public Drawable getDrawable(int id) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            return activity.getDrawable(id);
+        else
+            //noinspection deprecation
+            return activity.getResources().getDrawable(id);
+    }
+
+    public int getColor(int id) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            return activity.getColor(id);
+        else
+            //noinspection deprecation
+            return activity.getResources().getColor(id);
+    }
+
+}
